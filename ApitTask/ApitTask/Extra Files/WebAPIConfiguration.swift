f750a40972d0d5efbac47066c722a1b591952762
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-11-15 17:17:50 +0200

    Added Get Categories Web Service API Configuration Function

---
+++ ApitTask/ApitTask/Extra Files/WebAPIConfiguration.swift
@@ -0,0 +1,13 @@
+//
+//  WebAPIConfiguration.swift
+//  ApitTask
+//
+//  Created by Ahmed Elbasha on 11/15/18.
+//  Copyright © 2018 Ahmed Elbasha. All rights reserved.
+//
+
+import UIKit
+
+func getCategoriesWebAPI() -> String {
+    return "http://107.180.85.183:888/api/TaskDropdown?type=Category"
+}
